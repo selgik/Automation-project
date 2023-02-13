7780c3b88faf2b853609e89911b9653f89e40cb8
Sylvia K <91002274+selgik@users.noreply.github.com>
2023-02-13 21:50:36 +0800

    Update python_script.py

--- python_script.py
+++ python_script.py
@@ -4,23 +4,27 @@ import webbrowser
 import pyautogui
 
 
-#2. OPEN TIME APP
-subprocess.call(["open", "-a", "TIME"])
+#2. OPEN APP TO RECORD TIME 
+subprocess.call(["open", "-a", "PUNCH"])
+
+# Note: App named "PUNCH" is not an actual app; just replace "PUNCH" with actual app you'd like to open
 
 
-#3. ASK IF USER HAS PUNCHED IN, BEFORE STARTING A TIMER 
-pyautogui.alert("PUNCHED IN? Let me remind you when to punch out :)")
+#3. ASK IF USER HAS RECORDED TIME, BEFORE STARTING A TIMER 
+pyautogui.alert("Clocked in? Let me remind you when to clock out :)")
 
-# Note: Alternative: instead of pyautogui.alert, use pyautogui.confirm to give options [OK / CANCEL]
+# Note: Alternatively, use pyautogui.confirm instead of pyautogui.alert, to give options [OK / CANCEL]
+# 	Then, above code can be re-written as below:
 # answer = pyautogui.confirm("PUNCHED IN? Let me remind you when to punch out :)")
 # if answer == 'Cancel':
 #     quit()
 
 
-#4. CLOSE TIME APP
-subprocess.run(["killall", "TIME"])
+#4. CLOSE TIME-RECORD APP
+subprocess.run(["killall", "PUNCH"])
 
-# Note: alert box will not be closed. It will stay running as a proof that RPA has not been completed.
+# Note: alert box will not be closed until script is finished. (whole steps are done)
+# It will stay running as a proof that RPA has not been completed.
 # Tip: how to manually shut RPA? use below to force shut the execution:
 # Command + Option + Esc
 
@@ -33,19 +37,19 @@ webbrowser.open("https://www.web4.com")
 webbrowser.open("https://www.web5.com")
 
 
-#6. REMIND USER: 5 MIN BEFORE PUNCHING OUT (EX. USER TAKING 45MIN LUNCH):
+#6. REMIND USER: 5 MIN BEFORE CLOCK-OUT (EX. USER TAKING 1H LUNCH):
 ### Apple Script has been inserted
 
 def reminder():
     cmd = """
-    set time1 to (minutes of (current date)) + 520
+    set time1 to (minutes of (current date)) + 535
     repeat
 	delay 1
 	if minutes of (current date) = time1 then
 		
 		set variableWithSoundName to "Sonar"
 		tell application "Finder" to activate
-		display notification "5 min left to punch out!" with title "PUNCH OUT" sound name variableWithSoundName
+		display notification "5 min left to clock out!" with title "CLOCK OUT" sound name variableWithSoundName
                 return
 	end if
     end repeat
@@ -56,7 +60,7 @@ def reminder():
 print(reminder())
 
 
-#7. REMIND USER: 1 MIN BEFORE PUNCHING OUT
+#7. REMIND USER: 1 MIN BEFORE CLOCK-OUT
 ### Apple Script has been inserted
 
 def reminder2():
@@ -68,7 +72,7 @@ def reminder2():
     	
 		set variableWithSoundName to "Sonar"
 		tell application "Finder" to activate
-		display notification "1 min left to punch out!" with title "PUNCH OUT" sound name variableWithSoundName
+		display notification "1 min left to clock out!" with title "CLOCK OUT" sound name variableWithSoundName
                 return
 	end if
     end repeat
