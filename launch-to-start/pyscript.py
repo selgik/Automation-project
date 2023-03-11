dd11e3f4a16b9b145cf6fb3e933b62621b6943eb
Sylvia K <91002274+selgik@users.noreply.github.com>
2023-03-11 10:32:08 +0800

    Update and rename pyscript(autostart).py to launch-to-start/pyscript.py

--- launch-to-start/pyscript.py
+++ launch-to-start/pyscript.py
@@ -5,25 +5,24 @@ import pyautogui
 
 
 #2. OPEN APP TO RECORD TIME 
+# Note: "PUNCH" is not an actual app; replace "PUNCH" with actual app name you'd like to open
 subprocess.call(["open", "-a", "PUNCH"])
-
-# Note: "PUNCH" is not an actual app; just replace "PUNCH" with actual app name you'd like to open
 
 
 #3. ASK IF USER HAS RECORDED TIME, BEFORE STARTING A TIMER 
 pyautogui.alert("Clocked in? Let me remind you when to clock out :)")
 
 # Note: Alternatively, use pyautogui.confirm instead of pyautogui.alert, to give options [OK / CANCEL]
-# 	Then, above code can be re-written as below:
-# answer = pyautogui.confirm("PUNCHED IN? Let me remind you when to punch out :)")
-# if answer == 'Cancel':
-#     quit()
+# 	If so, above code can be re-written as below:
+# 	answer = pyautogui.confirm("PUNCHED IN? Let me remind you when to punch out :)")
+# 	if answer == 'Cancel':
+#     	quit()
 
 
 #4. CLOSE TIME-RECORD APP
 subprocess.run(["killall", "PUNCH"])
 
-# Note: alert box will not be closed until script is finished. (whole steps are done)
+# Note: alert box will not be closed until script is finished. (until whole steps are done)
 # It will stay running as a proof that RPA has not been completed.
 # Tip: how to manually shut RPA? use below to force shut the execution:
 # Command + Option + Esc
